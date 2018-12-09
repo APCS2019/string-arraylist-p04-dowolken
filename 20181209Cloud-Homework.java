df1918e859738cfec7d4c508e615837d06d2e96a
dowolken <45375823+dowolken@users.noreply.github.com>
2018-12-10 06:46:59 +0800

    Update 20181209Cloud-Homework.java

--- 20181209Cloud-Homework.java
+++ 20181209Cloud-Homework.java
@@ -1,1 +1,13 @@
-public static
+
+public String decodeString(ArrayList<StringPart> parts){
+  String str="";
+  for(int i=0;i<parts.size();i++)
+     {str=str+masterString.substring(part.get(i).getStart(),(part.get(i).getStart()+part.get(i).getLength()));}
+  return str;
+}
+public ArrayList<StringPart> encodeString(String word){
+  
+
+  
+
+      
