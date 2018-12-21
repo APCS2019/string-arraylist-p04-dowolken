5af4d04234666f66a39abda53641fac9f73b8626
dowolken <45375823+dowolken@users.noreply.github.com>
2018-12-21 21:22:49 +0800

    Update 20181209Cloud-Homework.java

--- 20181209Cloud-Homework.java
+++ 20181209Cloud-Homework.java
@@ -9,13 +9,13 @@ public ArrayList<StringPart> encodeString(String word){
   int i=0;
   ArrayList<StringPart> arr = new ArrayList<StringPart>();
   StringPart a;
-  while(!i.equals(word.length())){
+  while(word.length()!=0){
     a=findPart(word);
-    i=i+a.getLength();
     arr.add(a);
-    if(i<word.length())
-       {word=word.substring(a.getStart()+a.getLength());}
+    word=word.substring(a.getLength());}
   }
+  return arr;
+                            
 }
     
   
